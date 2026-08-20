a28d2bf0a0201e747c3f83b0aeb3b2fb2aa26238
TejasBal1607 <tejas1607.best@gmail.com>
2026-08-20 19:06:11 +0530

    Set Matrix Zeroes

---
+++ Python/set_matrix_zero.py
@@ -0,0 +1,31 @@
+n, m = map(int, input().split())
+matrix = []
+for i in range(n):
+    matrix.append(list(map(int, input().split())))
+
+col0 = 1
+for i in range(n):
+    for j in range(m):
+        if matrix[i][j] == 0:
+            matrix[i][0] = 0
+            if j!=0:
+                matrix[0][j] = 0
+            else:
+                col0 = 0
+
+for i in range(1,n):
+    for j in range(1,m):
+        if matrix[i][j] != 0:
+            if matrix[i][0] == 0 or matrix[0][j] == 0:
+                matrix[i][j] = 0
+
+if matrix[0][0] == 0:
+    for j in range(m):
+        matrix[0][j] = 0
+
+if col0 == 0:
+    for i in range(n):
+        matrix[i][0] = 0
+
+for i in matrix:
+    print(i)
