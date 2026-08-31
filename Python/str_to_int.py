35344b9db4bc39f946b348e90e102e02519ab166
TejasBal1607 <tejas1607.best@gmail.com>
2026-08-31 17:57:35 +0530

    String to Integer conversion (atoi)

---
+++ Python/str_to_int.py
@@ -0,0 +1,32 @@
+s = input()
+s = s.lstrip()
+
+if not s:
+    print(0)
+
+sign = 1
+i = 0
+
+if s[0] == "-":
+    sign = -1
+    i += 1
+elif s[0] == "+":
+    i += 1
+
+result = 0
+
+while i < len(s) and s[i].isdigit():
+    result = result * 10 + int(s[i])
+    i += 1
+
+result *= sign
+
+INT_MAX = 2**31 - 1
+INT_MIN = -(2**31)
+
+if result > INT_MAX:
+    print(INT_MAX)
+elif result < INT_MIN:
+    print(INT_MIN)
+else:
+    print(result)
